6d871ed1ecafd5e2f63ebf6d027dad8b722f6ee6
Amirreza <atnniya@gmail.com>
2017-05-06 12:37:26 -0400

    initilizing app js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-    console.log('its working fine ')
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-})
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.listen(3000, function () {
-  console.log('Server has started successfully')
-})
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.send('hello world');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
